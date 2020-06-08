965f1b43e952d62fe51988788b68d3c9c579bd7f
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-06-09 00:32:46 +0530

    fix animation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let values
+let values,
+  i = 0,
+  j = 0
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight)
@@ -6,20 +8,22 @@ function setup() {
   for (let i = 0; i < values.length; i++) {
     values[i] = Math.floor(random(height))
   }
+}
 
-  for (let i = 0; i < values.length; i++) {
-    for (let j = 0; j < values.length - i - 1; j++) {
+function draw() {
+  background(0)
+  if (i < values.length) {
+    while (j < values.length - i - 1) {
       let a = values[j]
       let b = values[j + 1]
       if (a > b) {
         swap(values, j, j + 1)
       }
+      j++
     }
   }
-}
-
-function draw() {
-  background(0)
+  j = 0
+  i++
   for (let i = 0; i < values.length; i++) {
     stroke(255)
     line(i, height, i, height - values[i])
